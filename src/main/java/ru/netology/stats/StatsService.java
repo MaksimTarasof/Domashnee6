0b95d6f4ce83f86a5467ac1648c7b51bfd6c1c26
Max <eparis@atlassian.com>
2023-01-22 18:11:17 +0400

    first commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,69 @@
+package ru.netology.stats;
+
+public class StatsService {
+    //1 Сумма всех продаж
+    public int allSales(int[] sales) {
+
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+        }
+        return sum;
+    }
+
+    //2 Средняя сумма продаж в месяц
+    public int averageSales(int[] sales) {
+
+        double sum = 0;
+        for (int i : sales) {
+            sum += i;
+        }
+        return (int) (sum / sales.length);
+    }
+
+    //3 Номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму*.
+    public int maxSales(int[] sales) {
+        int maxMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return maxMonth + 1;
+    }
+
+    //4
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    //5 Количество месяцев, в которых продажи были ниже среднего
+    public int belowTheAverageSales(int[] sales) {
+
+        int month = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (i < averageSales(sales)) {
+                month++;
+            }
+
+        }
+        return month;
+    }
+
+}
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,78 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test //1
+
+    public void shouldFindAllSales() {
+
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.allSales(sales);
+        int expected = 180;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //2
+
+    public void shouldFindAverageSales() {
+
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.averageSales(sales);
+        int expected = 15;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //3
+
+    public void shouldFindMaxMounth() {
+
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.maxSales(sales);
+        int expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //4
+
+    public void shouldFindBetweenEnds() {
+
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.minSales(sales);
+        int expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test //5
+
+    public void lowAwg() {
+
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int actual = service.belowTheAverageSales(sales);
+        int expected = 12;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
